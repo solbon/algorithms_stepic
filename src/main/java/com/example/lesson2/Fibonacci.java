6e2af4f97190e7dc33101ea01fe960c6fecee324
solbon <solbon@gmail.com>
2017-11-30 16:14:51 +0100

    Fast Fibonacci algorithm

--- src/main/java/com/example/lesson2/Fibonacci.java
+++ src/main/java/com/example/lesson2/Fibonacci.java
@@ -5,31 +5,27 @@ import java.util.Scanner;
 public class Fibonacci {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String n;
-        do {
-            System.out.print("Please input number: ");
-            n = scanner.nextLine();
-            if (n.equals("q")) {
-                break;
-            }
-            try {
-                Integer i = Integer.valueOf(n);
-                if (i < 0) {
-                    System.out.println("Please input positive number");
-                } else {
-                    System.out.println(calculateFibonacci(i));
-                }
-            } catch (NumberFormatException e) {
-                System.out.println("Only number or 'q' to exit");
-            }
-        } while (!n.equals("q"));
+        int n = scanner.nextInt();
+        System.out.println(fastFibonacci(n));
     }
 
     private static long calculateFibonacci(int n) {
-        switch (n) {
-            case 0: return 0;
-            case 1: return 1;
+        if (n <= 2) {
+            return 1;
         }
         return calculateFibonacci(n - 1) + calculateFibonacci(n - 2);
+    }
+
+    private static long fastFibonacci(int n) {
+        if (n <= 2) {
+            return 1;
+        }
+        long[] arr = new long[n + 1];
+        arr[0] = 0;
+        arr[1] = 1;
+        for(int i = 2; i <= n; i++) {
+            arr[i] = arr[i - 1] + arr[i - 2];
+        }
+        return arr[n];
     }
 }
