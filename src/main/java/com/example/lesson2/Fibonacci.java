ef8b1fae35ab29eceb5076fc2db8f2ddb55ca7bf
solbon <solbon@gmail.com>
2017-12-01 08:23:40 +0100

    Last digit of N Fibonacci

--- src/main/java/com/example/lesson2/Fibonacci.java
+++ src/main/java/com/example/lesson2/Fibonacci.java
@@ -6,7 +6,7 @@ public class Fibonacci {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        System.out.println(fastFibonacci(n));
+        System.out.println(lastDigitFibonacci(n));
     }
 
     private static long calculateFibonacci(int n) {
@@ -25,6 +25,19 @@ public class Fibonacci {
         arr[1] = 1;
         for(int i = 2; i <= n; i++) {
             arr[i] = arr[i - 1] + arr[i - 2];
+        }
+        return arr[n];
+    }
+
+    private static int lastDigitFibonacci(int n) {
+        if (n < 2) {
+            return n;
+        }
+        int arr[] = new int[n + 1];
+        arr[0] = 0;
+        arr[1] = 1;
+        for(int i = 2; i <= n; i++) {
+            arr[i] = (arr[i - 1] + arr[i - 2]) % 10;
         }
         return arr[n];
     }
